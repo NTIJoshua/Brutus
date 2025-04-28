1bb17550da222af62d532b108fe7391416e93e96
NTIJoshua <joshua.odogwu@elev.ga.ntig.se>
2025-04-28 12:13:57 +0200

    Co-authored-by: JimNTI <JimNTI@users.noreply.github.com>

--- Brutus.rb
+++ Brutus.rb
@@ -1,4 +1,14 @@
 class AdventureGame
+  COLORS = {
+    red: "\e[31m",
+    green: "\e[32m",
+    yellow: "\e[33m",
+    blue: "\e[34m",
+    magenta: "\e[35m",
+    cyan: "\e[36m",
+    reset: "\e[0m"
+  }
+
   def initialize
     @game_over = false
     @current_room = :battlefield
@@ -15,7 +25,20 @@ class AdventureGame
     @strength = rand(5..10)
     @time = "afternoon"
     @day = 1
+    @enemies = {
+      "stragglers" => { hp: 15, damage: 3, xp: 10 },
+      "imperial_scout" => { hp: 20, damage: 5, xp: 15 },
+      "mercenary" => { hp: 25, damage: 6, xp: 20 }
+    }
   end
+
+  def colorize(text, color)
+  "#{COLORS[color]}#{text}#{COLORS[:reset]}"
+  end
+
+def player_name
+  colorize(@playername, :yellow)
+end
 
   def start
     character_creation
@@ -30,9 +53,9 @@ class AdventureGame
     puts "What's your name, kid?"
     puts "*He stretches out his war-worn hand.*"
     @playername = gets.chomp
-    puts "*You grab his hand and pull yourself up*"
+    puts "#{colorize(*'You grab his hand and pull yourself up*', :green)}"
     sleep 4
-    puts "My name is #{@playername}."
+    puts "My name is #{player_name}."
     sleep 4
     while @playername.length > 10
       puts "What?? That's too long!"
@@ -45,25 +68,25 @@ class AdventureGame
     end
     case @playername[0]&.upcase
     when "S"
-      puts "#{@playername}, huh? Strong name. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
+      puts "#{player_name}, huh? Strong name. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
     when "J"
-      puts "#{@playername}, huh? An honorable name. I can tell you'll be great. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
+      puts "#{player_name}, huh? An honorable name. I can tell you'll be great. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
     when "H"
-      puts "#{@playername}, huh? A name of vice. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
+      puts "#{player_name}, huh? A name of vice. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
     when "A"
-      puts "#{@playername}, huh? A wise name. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
+      puts "#{player_name}, huh? A wise name. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
     when "N"
-      puts "#{@playername}, huh? An interesting name. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
+      puts "#{player_name}, huh? An interesting name. Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
     else
-      puts "#{@playername}, huh? Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
+      puts "#{player_name}, huh? Don't worry, I've been on more battlefields than you can count. I'll get you out of here."
     end
   end
 
   def introduction_dialogue
     sleep 4
-    puts "\nWho am I? The name's Robbert, a mercenary, nice to meet'cha."
+    puts "\nWho am I? The name's #{colorize('Robbert', :blue)}, a mercenary, nice to meet'cha."
     sleep 4
-    puts "Robbert: By the way, how'd you get in this mess? How old are you even?"
+    puts "#{colorize('Robbert', :blue)}: By the way, how'd you get in this mess? How old are you even?"
     sleep 4
     
     # Player's response options
@@ -76,24 +99,24 @@ class AdventureGame
 
     case answer
     when 1
-      puts "#{@playername}: I'm twelve. Both of my parents died from a massive boulder that fell from the sky when the invasion began."
+      puts "#{player_name}: I'm twelve. Both of my parents died from a massive boulder that fell from the sky when the invasion began."
       @game_state[:robert_trust] += 2
       @game_state[:boulder_event] = true
       sleep 4
     when 2
-      puts "#{@playername}: I'm... sixteen. I got separated from my unit."
+      puts "#{player_name}: I'm... sixteen. I got separated from my unit."
       @game_state[:robert_trust] -= 1
       sleep 4
     else
-      puts "#{@playername}: ..."
-      puts "Robbert: Tough kid, huh? Fair enough."
+      puts "#{player_name}: ..."
+      puts "#{colorize('Robbert', :blue)}: Tough kid, huh? Fair enough."
       sleep 4
     end
     
     sleep 4
-    puts "Robbert: ...My condolences. Then again, you're probably not the only one."
+    puts "#{colorize('Robbert', :blue)}: ...My condolences. Then again, you're probably not the only one."
     sleep 4
-    puts "Robbert: Wouldn't surprise me if the churches were working their asses off right now."
+    puts "#{colorize('Robbert', :blue)}: Wouldn't surprise me if the churches were working their asses off right now."
     sleep 4
     puts "\nThe battlefield stretches before you - smoke rises from distant craters, and the sounds of distant combat echo."
     @game_state[:knows_about_invasion] = true
@@ -118,8 +141,7 @@ class AdventureGame
     when :boulder_site
       puts "\nThe Skyfall Impact Site"
       puts "The massive meteor-like boulder dominates the landscape. Its surface is unnaturally smooth."
-      puts "Near its base, you spot two figures in noble garb - unmoving."
-      
+      puts "Near its base, you spot two figures in noble garb - unmoving."  
     end
   end
 
@@ -143,7 +165,7 @@ class AdventureGame
         puts "Their noble crest, the same as your pendant, glints in the dim light."
         @game_state[:found_parents] = true
         @game_state[:robert_trust] += 5
-        puts "Robbert puts a firm hand on your shoulder: 'We'll make them pay for this.'"
+        puts "#{colorize('Robbert', :blue)} puts a firm hand on your shoulder: 'We'll make them pay for this.'"
         return true
       end
     end
@@ -173,6 +195,11 @@ class AdventureGame
       when 'use'
         item = params.join(' ')
         use_item(item)
+      when 'equip'
+        item = params.join(' ')
+        equip_weapon(item)
+      when 'inventory', 'items'
+        show_inventory
       when 'attack'
         target = params.join(' ')
         combat(target)
@@ -188,7 +215,7 @@ class AdventureGame
         puts "I don't understand that command."
       end
     end
-    puts "Thanks for playing, #{@playername}."
+    puts "Thanks for playing, #{player_name}."
   end
 
   def move_to(direction)
@@ -196,7 +223,14 @@ class AdventureGame
     when [:battlefield, "toward"], [:battlefield, "fighting"]
       puts "You move toward the sounds of clashing steel..."
       initiate_combat("stragglers")
-    when [:boulder_site, "return"]
+    when [:battlefield, "northeast"], [:battlefield, "ne"]
+      if @game_state[:boulder_event] && !@game_state[:found_parents]
+        @current_room = :boulder_site
+        puts "You approach the massive boulder that fell from the sky..."
+      else
+        puts "You don't see anything interesting in that direction."
+      end
+    when [:boulder_site, "return"], [:boulder_site, "back"], [:boulder_site, "battlefield"]
       @current_room = :battlefield
       puts "You return to the main battlefield."
     else
@@ -207,16 +241,27 @@ class AdventureGame
   def handle_dialogue(topic)
     case topic.downcase
     when "invasion"
-      puts "Robbert: The Eastern Empire struck at dawn with those damned sky-boulders first."
+      puts "#{colorize('Robbert', :blue)}: The Eastern Empire struck at dawn with those damned sky-boulders first."
       puts "Never seen anything like it. Took out half the noble houses before the fighting even started."
     when "parents"
       if @game_state[:found_parents]
-        puts "Robbert: Your parents... they were targeted specifically. This was no random attack."
+        puts "#{colorize('Robbert', :blue)}: Your parents... they were targeted specifically. This was no random attack."
       else
-        puts "Robbert: We'll find them, kid. But first we need to survive."
+        puts "#{colorize('Robbert', :blue)}: We'll find them, kid. But first we need to survive."
+      end
+    when "#{colorize('Robbert', :blue)}", "robert", "yourself"
+      puts "#{colorize('Robbert', :blue)}: Me? Just a hired sword caught in the wrong place at the wrong time."
+      puts "I've fought in three wars before this one, but nothing like... this."
+      @game_state[:robert_trust] += 1
+    when "boulder", "sky-boulder", "meteor"
+      if @game_state[:boulder_event]
+        puts "#{colorize('Robbert', :blue)}: Those things aren't natural. Empire's got some new weapon."
+        puts "They're too smooth, too precise. And they always hit important targets."
+      else
+        puts "#{colorize('Robbert', :blue)}: What boulder are you talking about?"
       end
     else
-      puts "Robbert: Not now, kid. Stay focused."
+      puts "#{colorize('Robbert', :blue)}: Not now, kid. Stay focused."
     end
   end
 
@@ -228,13 +273,114 @@ class AdventureGame
       @time = "dawn"
       @day += 1
       puts "A new day breaks over the devastated battlefield..."
+    when "dawn" then @time = "morning"
+    when "morning" then @time = "afternoon"
     end
     puts "Time passes... It's now #{@time}."
+    
+    # Random encounter chance
+    if rand(1..10) > 7
+      enemy_types = ["stragglers", "imperial_scout", "mercenary"]
+      enemy = enemy_types.sample
+      initiate_combat(enemy)
+    end
   end
 
   def initiate_combat(enemy)
     puts "\nYou encounter #{enemy}! Battle begins!"
-    # Your combat system would trigger here
+    enemy_stats = @enemies[enemy]
+    enemy_hp = enemy_stats[:hp]
+    
+    while enemy_hp > 0
+      puts "\nEnemy HP: #{enemy_hp}"
+      puts "Your HP: #{@strength}"
+      puts "\nWhat will you do?"
+      puts "1. Attack"
+      puts "2. Run"
+      print "> "
+      choice = gets.chomp.to_i
+      
+      case choice
+      when 1
+        player_damage = current_weapon[:damage] + rand(1..3)
+        puts "You attack with your #{current_weapon[:name]} and deal #{player_damage} damage!"
+        enemy_hp -= player_damage
+        
+        if enemy_hp <= 0
+          puts "You defeated the #{enemy}!"
+          gain_experience(enemy_stats[:xp])
+          loot_chance(enemy)
+          break
+        end
+        
+        enemy_damage = enemy_stats[:damage] + rand(0..2)
+        puts "The #{enemy} attacks you for #{enemy_damage} damage!"
+        @strength -= enemy_damage
+        
+        if @strength <= 0
+          puts "You have been defeated..."
+          @game_over = true
+          break
+        end
+      when 2
+        escape_chance = rand(1..10)
+        if escape_chance > 3
+          puts "You managed to escape!"
+          break
+        else
+          puts "You couldn't escape!"
+          enemy_damage = enemy_stats[:damage] + rand(0..2)
+          puts "The #{enemy} attacks you for #{enemy_damage} damage!"
+          @strength -= enemy_damage
+          
+          if @strength <= 0
+            puts "You have been defeated..."
+            @game_over = true
+            break
+          end
+        end
+      else
+        puts "Invalid choice!"
+      end
+    end
+  end
+
+  def loot_chance(enemy)
+    chance = rand(1..10)
+    case enemy
+    when "stragglers"
+      if chance > 7 && !@player_inventory.include?("bandages")
+        puts "You found some bandages!"
+        @player_inventory << "bandages"
+      end
+    when "imperial_scout"
+      if chance > 5 && !@player_inventory.include?("spear")
+        puts "You found an Imperial Spear!"
+        @player_inventory << "spear"
+      end
+    when "mercenary"
+      if chance > 3 && !@player_inventory.include?("pendant")
+        puts "You found a noble's pendant!"
+        @player_inventory << "pendant"
+      end
+    end
+  end
+
+  def gain_experience(xp)
+    @experience += xp
+    puts "You gained #{xp} experience points!"
+    
+    if @experience >= @level * 10
+      level_up
+    end
+  end
+
+  def level_up
+    @level += 1
+    @strength += 2
+    @experience = 0
+    puts "You leveled up! You are now level #{@level}!"
+    puts "Your strength increased to #{@strength}!"
   end
 
   def show_inventory
@@ -253,6 +399,19 @@ class AdventureGame
         puts "You take a stance, imagining an enemy. The only thing they fear is you."
       when 'spear'
         puts "The spear was finely crafted, are you capable of using it to its fullest potential?"
+      when 'bandages'
+        heal_amount = rand(3..5)
+        @strength += heal_amount
+        @player_inventory.delete('bandages')
+        puts "You use the bandages to heal #{heal_amount} health. Your strength is now #{@strength}."
+      when 'pendant'
+        if @game_state[:found_parents]
+          puts "You look at the pendant, matching your parents' crest. A tear rolls down your cheek."
+          puts "#{colorize('Robbert', :blue)}: We'll avenge them, I promise you that."
+          @game_state[:robert_trust] += 1
+        else
+          puts "A noble pendant. It seems oddly familiar."
+        end
       else
         puts "You use the #{item}, but nothing happens."
       end
@@ -263,51 +422,79 @@ class AdventureGame
 
   def equip_weapon(item)
     weapons = ['spear', 'sword']
-    if @player_inventory.include?(item)
-      case item
-      when weapons.include?(item)
-        puts "You equip the #{item}."
-      end
+    if @player_inventory.include?(item) && weapons.include?(item)
+      puts "You equip the #{item}."
+      @equipped_weapon = item
+    else
+      puts "You can't equip that."
     end
-    puts "I can't equip that."
   end
 
   def take_item(item)
     case [@current_room, item]
     when [:battlefield, 'sword']
-      @player_inventory << 'sword'
-      puts "You take the Dulled sword."
+      if !@player_inventory.include?('sword')
+        @player_inventory << 'sword'
+        puts "You take the Dulled sword."
+      else
+        puts "You already have a sword."
+      end
     when [:battlefield, 'spear']
-      @player_inventory << 'spear'
-      puts "You take the spear."
+      if !@player_inventory.include?('spear')
+        @player_inventory << 'spear'
+        puts "You take the spear."
+      else
+        puts "You already have a spear."
+      end
+    when [:boulder_site, 'pendant']
+      if @game_state[:found_parents] && !@player_inventory.include?('pendant')
+        @player_inventory << 'pendant'
+        puts "You take your family pendant from your parents' remains."
+        puts "#{colorize('Robbert', :blue)} watches silently, his face grim with determination."
+      else
+        puts "I don't see that here."
+      end
     else
       puts "I don't see that here."
     end
   end
 
-
   def show_stats
-    puts "#{@playername}, Level #{@level}"
+    puts "#{player_name}, Level #{@level}"
     puts "Strength: #{@strength}"
+    puts "Experience: #{@experience}/#{@level * 10}"
     puts "Weapon: #{current_weapon[:name]} (#{current_weapon[:damage]} damage)"
     puts "Robert's Trust: #{@game_state[:robert_trust]}"
+    puts "Current time: #{@time.capitalize}, Day #{@day}"
   end
 
   def current_weapon
-    @player_inventory.include?("sword") ? {name: "Sword", damage: 8} : {name: "Fists", damage: 3}
+    if @player_inventory.include?("spear") && @equipped_weapon == "spear"
+      {name: "Spear", damage: 10}
+    elsif @player_inventory.include?("sword") && (@equipped_weapon == "sword" || @equipped_weapon.nil?)
+      {name: "Sword", damage: 8}
+    else
+      {name: "Fists", damage: 3}
+    end
   end
 
   def show_help
     puts "Available commands:"
     puts "look - Describe current location"
-    puts "move [direction] - Move to new area"
+    puts "go/move [direction] - Move to new area"
     puts "search - Look for items"
-    puts "talk [topic] - Ask Robbert about something"
+    puts "examine - Examine something interesting"
+    puts "approach - Get closer to something"
+    puts "talk/ask [topic] - Ask #{colorize('Robbert', :blue)} about something"
+    puts "take/get [item] - Pick up an item"
+    puts "use [item] - Use an item from your inventory"
+    puts "equip [weapon] - Equip a weapon"
+    puts "inventory/items - Show your inventory"
     puts "attack - Fight enemies"
     puts "wait - Pass time"
     puts "stats - Show your status"
     puts "help - Show this menu"
-    puts "quit - End the game"
+    puts "quit/exit - End the game"
   end
 end
 
---
+++ dataLoader.rb
@@ -0,0 +1,300 @@
+class GameDataLoader
+  # Constants for file paths
+  INVENTORY_FILE = "data/inventory.txt"
+  STATS_FILE = "data/stats.txt"
+  ROOMS_FILE = "data/rooms.txt"
+  DIALOGUE_FILE = "data/dialogue.txt"
+  ENEMIES_FILE = "data/enemies.txt"
+  
+  # Load all game data at once
+  def self.load_all_data
+    {
+      inventory_items: load_inventory_items,
+      stats: load_stats,
+      rooms: load_rooms,
+      dialogues: load_dialogues,
+      enemies: load_enemies
+    }
+  end
+
+  # Load inventory items from file
+  def self.load_inventory_items
+    items = {}
+    if File.exist?(INVENTORY_FILE)
+      File.readlines(INVENTORY_FILE).each do |line|
+        next if line.strip.empty? || line.start_with?('#')
+        
+        # Expected format: item_id|name|description|type|effect_value
+        parts = line.strip.split('|')
+        if parts.length >= 5
+          item_id = parts[0]
+          items[item_id] = {
+            name: parts[1],
+            description: parts[2],
+            type: parts[3],
+            effect_value: parts[4].to_i
+          }
+        end
+      end
+    end
+    items
+  end
+
+  # Load player and NPC stats from file
+  def self.load_stats
+    stats = {}
+    if File.exist?(STATS_FILE)
+      current_entity = nil
+      
+      File.readlines(STATS_FILE).each do |line|
+        line = line.strip
+        next if line.empty? || line.start_with?('#')
+        
+        if line.start_with?('[') && line.end_with?(']')
+          # This is an entity name like [player] or [robert]
+          current_entity = line[1..-2].downcase
+          stats[current_entity] = {}
+        elsif current_entity && line.include?(':')
+          # This is a stat entry like "strength: 5"
+          key, value = line.split(':', 2).map(&:strip)
+          stats[current_entity][key.to_sym] = parse_value(value)
+        end
+      end
+    end
+    stats
+  end
+
+  # Load room descriptions and properties from file
+  def self.load_rooms
+    rooms = {}
+    if File.exist?(ROOMS_FILE)
+      current_room = nil
+      description = []
+      
+      File.readlines(ROOMS_FILE).each do |line|
+        line = line.strip
+        
+        if line.start_with?('==') && line.end_with?('==')
+          # New room definition
+          if current_room
+            # Save previous room before starting new one
+            rooms[current_room][:description] = description.join("\n") if description.any?
+          end
+          
+          current_room = line.gsub(/==|\s+/, '').downcase.to_sym
+          rooms[current_room] = { exits: {}, items: [] }
+          description = []
+        elsif line.start_with?('exit:') && current_room
+          # Room exit definition
+          _, direction, destination = line.split(':', 3).map(&:strip)
+          rooms[current_room][:exits][direction.downcase.to_sym] = destination.downcase.to_sym
+        elsif line.start_with?('item:') && current_room
+          # Item in room
+          item = line.sub('item:', '').strip
+          rooms[current_room][:items] << item
+        elsif !line.empty? && !line.start_with?('#') && current_room
+          # Description line
+          description << line
+        end
+      end
+      
+      # Save the last room's description
+      if current_room
+        rooms[current_room][:description] = description.join("\n") if description.any?
+      end
+    end
+    rooms
+  end
+  
+  # Load dialogue options from file
+  def self.load_dialogues
+    dialogues = {}
+    if File.exist?(DIALOGUE_FILE)
+      current_npc = nil
+      current_topic = nil
+      current_lines = []
+      
+      File.readlines(DIALOGUE_FILE).each do |line|
+        line = line.strip
+        next if line.empty? || line.start_with?('#')
+        
+        if line.start_with?('[') && line.end_with?(']')
+          # This is an NPC name like [robert]
+          save_current_dialogue(dialogues, current_npc, current_topic, current_lines)
+          current_npc = line[1..-2].downcase
+          dialogues[current_npc] ||= {}
+          current_topic = nil
+          current_lines = []
+        elsif line.start_with?('{') && line.end_with?('}') && current_npc
+          # This is a topic like {invasion}
+          save_current_dialogue(dialogues, current_npc, current_topic, current_lines)
+          current_topic = line[1..-2].downcase
+          dialogues[current_npc][current_topic] ||= { lines: [], requirements: {}, effects: {} }
+          current_lines = []
+        elsif line.start_with?('require:') && current_npc && current_topic
+          # Requirement for this dialogue
+          _, key, value = line.split(':', 3).map(&:strip)
+          dialogues[current_npc][current_topic][:requirements][key.to_sym] = parse_value(value)
+        elsif line.start_with?('effect:') && current_npc && current_topic
+          # Effect of this dialogue
+          _, key, value = line.split(':', 3).map(&:strip)
+          dialogues[current_npc][current_topic][:effects][key.to_sym] = parse_value(value)
+        elsif current_npc && current_topic
+          # Dialogue line
+          current_lines << line
+        end
+      end
+      
+      # Save the last dialogue
+      save_current_dialogue(dialogues, current_npc, current_topic, current_lines)
+    end
+    dialogues
+  end
+  
+  # Helper method to save dialogue during parsing
+  def self.save_current_dialogue(dialogues, npc, topic, lines)
+    if npc && topic && lines.any?
+      dialogues[npc] ||= {}
+      dialogues[npc][topic] ||= { lines: [], requirements: {}, effects: {} }
+      dialogues[npc][topic][:lines] = lines
+    end
+  end
+  
+  # Load enemy definitions from file
+  def self.load_enemies
+    enemies = {}
+    if File.exist?(ENEMIES_FILE)
+      current_enemy = nil
+      
+      File.readlines(ENEMIES_FILE).each do |line|
+        line = line.strip
+        next if line.empty? || line.start_with?('#')
+        
+        if line.start_with?('[') && line.end_with?(']')
+          # Enemy name like [stragglers]
+          current_enemy = line[1..-2].downcase
+          enemies[current_enemy] = {}
+        elsif current_enemy && line.include?(':')
+          # Property like "hp: 15"
+          key, value = line.split(':', 2).map(&:strip)
+          
+          if key == 'loot'
+            # Special handling for loot table
+            enemies[current_enemy][:loot] ||= []
+            item, chance = value.split(',').map(&:strip)
+            enemies[current_enemy][:loot] << {
+              item: item,
+              chance: chance.to_i
+            }
+          else
+            enemies[current_enemy][key.to_sym] = parse_value(value)
+          end
+        end
+      end
+    end
+    enemies
+  end
+  
+  # Helper method to parse values of different types
+  def self.parse_value(value)
+    return true if value.downcase == 'true'
+    return false if value.downcase == 'false'
+    return value.to_i if value =~ /^\d+$/
+    return value.to_f if value =~ /^\d+\.\d+$/
+    value # Return as string if no special type
+  end
+end
+
+# Example usage files
+def create_example_files
+  FileUtils.mkdir_p('data')
+  
+  # Inventory items example
+  File.open('data/inventory.txt', 'w') do |f|
+    f.puts "# Inventory Items Format: item_id|name|description|type|effect_value"
+    f.puts "sword|Dulled Sword|A dulled sword found on the battlefield.|weapon|8"
+    f.puts "spear|Imperial Spear|A well-crafted spear of the Imperial army.|weapon|10"
+    f.puts "bandages|Bandages|Medical bandages that can heal wounds.|healing|4"
+    f.puts "pendant|Noble Pendant|A pendant bearing your family crest.|key_item|0"
+  end
+  
+  # Stats example
+  File.open('data/stats.txt', 'w') do |f|
+    f.puts "# Character Stats"
+    f.puts "[player]"
+    f.puts "level: 1"
+    f.puts "experience_cap: 10"
+    f.puts "base_strength: 8"
+    
+    f.puts "[robert]"
+    f.puts "base_trust: 0"
+    f.puts "max_trust: 10"
+  end
+  
+  # Rooms example
+  File.open('data/rooms.txt', 'w') do |f|
+    f.puts "# Room Definitions"
+    f.puts "==battlefield=="
+    f.puts "A horrendous sight."
+    f.puts "You start to wonder if it was all really worth it in the end."
+    f.puts "A mercenary is staring at you from the ground."
+    f.puts "And another."
+    f.puts "And another."
+    f.puts "You hold your head high."
+    f.puts "exit: northeast: boulder_site"
+    f.puts "item: sword"
+    
+    f.puts "==boulder_site=="
+    f.puts "The Skyfall Impact Site"
+    f.puts "The massive meteor-like boulder dominates the landscape. Its surface is unnaturally smooth."
+    f.puts "Near its base, you spot two figures in noble garb - unmoving."
+    f.puts "exit: back: battlefield"
+    f.puts "item: pendant"
+  end
+  
+  # Dialogue example
+  File.open('data/dialogue.txt', 'w') do |f|
+    f.puts "# NPC Dialogues"
+    f.puts "[robert]"
+    f.puts "{invasion}"
+    f.puts "The Eastern Empire struck at dawn with those damned sky-boulders first."
+    f.puts "Never seen anything like it. Took out half the noble houses before the fighting even started."
+    f.puts "effect: knows_about_invasion: true"
+    
+    f.puts "{parents}"
+    f.puts "require: found_parents: true"
+    f.puts "Your parents... they were targeted specifically. This was no random attack."
+    f.puts "effect: robert_trust: +1"
+    
+    f.puts "{parents_default}"
+    f.puts "require: found_parents: false"
+    f.puts "We'll find them, kid. But first we need to survive."
+    
+    f.puts "{boulder}"
+    f.puts "require: boulder_event: true"
+    f.puts "Those things aren't natural. Empire's got some new weapon."
+    f.puts "They're too smooth, too precise. And they always hit important targets."
+  end
+  
+  # Enemies example
+  File.open('data/enemies.txt', 'w') do |f|
+    f.puts "# Enemy Definitions"
+    f.puts "[stragglers]"
+    f.puts "hp: 15"
+    f.puts "damage: 3"
+    f.puts "xp: 10"
+    f.puts "loot: bandages, 30"
+    
+    f.puts "[imperial_scout]"
+    f.puts "hp: 20"
+    f.puts "damage: 5"
+    f.puts "xp: 15"
+    f.puts "loot: spear, 50"
+    
+    f.puts "[mercenary]"
+    f.puts "hp: 25"
+    f.puts "damage: 6"  
+    f.puts "xp: 20"
+    f.puts "loot: pendant, 70"
+  end
+end
